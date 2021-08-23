6f8fee6de6f8d2f642d4c282b79fa8eaa78a0770
renzhentaxi baerde <renzhentaxibaerde@renzhentaxisMBP.home>
2021-08-23 16:25:17 -0400

    nvim init setup

---
+++ nvim/init.lua
@@ -0,0 +1,5 @@
+require('plugins')
+
+require('compe_setup')
+require('lsp_setup')
+require('general_setup')
---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,6 @@
+return require('packer').startup(function()
+	use 'wbthomason/packer.nvim'
+	use 'neovim/nvim-lspconfig'
+	use 'hrsh7th/nvim-compe'
+	use 'folke/tokyonight.nvim'
+end)
