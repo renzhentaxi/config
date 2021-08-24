427e922bd24678a4285f509dd2f7a34c10ae38ca
renzhentaxi baerde <renzhentaxibaerde@renzhentaxisMBP.home>
2021-08-23 20:01:10 -0400

    zsh config

---
+++ zsh/general.zsh
@@ -0,0 +1,16 @@
+alias vi=nvim
+
+export XDG_CONFIG_HOME=~/.config
+export EDITOR='nvim'
+
+source $XDG_CONFIG_HOME/zsh/nvm.zsh
+
+#simple prompt
+source $XDG_CONFIG_HOME/zsh/pure.zsh
+#theme = snazzy
+
+#highlghts stuff in zsh shell
+source $HOME/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+bindkey -v
+
