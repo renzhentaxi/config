2bb7390a950d0567ddaf27236a182ab6eea64888
renzhentaxi baerde <renzhentaxibaerde@gmail.com>
2021-11-05 12:36:57 -0400

    added pyenv support

--- zsh/general.zsh
+++ zsh/general.zsh
@@ -16,3 +16,18 @@ source $HOME/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
 bindkey -v
 
+# Add pyenv executable to PATH and
+# enable shims by adding the following
+# to ~/.profile and ~/.zprofile:
+
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init --path)"
+
+# Load pyenv into the shell by adding
+# the following to ~/.zshrc:
+
+eval "$(pyenv init -)"
+
+# Make sure to restart your entire logon session
+# for changes to profile files to take effect.
