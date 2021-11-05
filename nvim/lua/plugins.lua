b015abefce953c224887bcae4c3a38a707d381db
renzhentaxi baerde <renzhentaxibaerde@gmail.com>
2021-11-05 12:37:12 -0400

    added which-key and lightspeed

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -4,4 +4,6 @@ return require('packer').startup(function()
 	use 'hrsh7th/nvim-compe'
 	use 'folke/tokyonight.nvim'
 	use 'neoclide/vim-jsx-improve'
+	use 'ggandor/lightspeed.nvim'
+	use 'folke/which-key.nvim'
 end)
