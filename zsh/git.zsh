9e6431b4c45e4ec12e8906ec2839f914fbe40c7f
renzhentaxi baerde <renzhentaxibaerde@gmail.com>
2021-11-15 09:30:24 -0500

    supports both existing and new branch

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -1,15 +1,34 @@
-setopt shwordsplit 
-setopt completealiases
-
-checkout_gitwork()
+is_git_repo()
 {
+    git -C $1 rev-parse 2>/dev/null
+    return $?
+}
+
+git_worktree_checkout()
+{
+    if ! is_git_repo .; then
+        echo 'not in a git repo!'
+        return 1
+    fi
+
     worktreeDir=../$1
-    rest=$*[2,-1]
+    rest=($*[2,-1])
+
+    cd $(git worktree list | head -n 1 | cut -d " " -f1)
+    git pull
+
     git worktree add $worktreeDir $rest
-    ln -s $(pwd)/node_modules $worktreeDir/node_modules
+
+    sourceNodeModule=$(realpath node_modules)
+    targetNodeModule=$(realpath $worktreeDir/node_modules)
+    ln -s $sourceNodeModule $targetNodeModule
+
     cd $worktreeDir
     return
 }
 
-alias gwtc=checkout_gitwork
-alias gwtr=git worktree remove
+alias gwtc=git_worktree_checkout
+
+
+alias reload="source ~/.config/zsh/git.zsh"
+alias edit="nvim ~/.config/zsh/git.zsh"
