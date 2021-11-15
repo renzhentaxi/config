40518b425fe20b4d58b765e9ab2b43e48fcfa373
renzhentaxi baerde <renzhentaxibaerde@gmail.com>
2021-11-15 16:45:20 -0500

    added nullls for stylua and prettier

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,46 +1,75 @@
 local function treeSitterConfig()
-	require 'nvim-treesitter.configs'.setup {
+	require("nvim-treesitter.configs").setup({
 		highlight = {
 			enable = true,
 			additional_vim_regex_highlighting = false,
-			use_languagetree = true
-		}
-	}
+			use_languagetree = true,
+		},
+	})
 end
 
-
-
-
+-- bootstrap
 local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+	packer_bootstrap = fn.system({
+		"git",
+		"clone",
+		"--depth",
+		"1",
+		"https://github.com/wbthomason/packer.nvim",
+		install_path,
+	})
 end
 
-require('packer').startup(function()
-	use 'wbthomason/packer.nvim'
-	use 'neovim/nvim-lspconfig'
-	use 'hrsh7th/nvim-compe'
-	use 'neoclide/vim-jsx-improve'
-	use 'ggandor/lightspeed.nvim'
-	use 'folke/which-key.nvim'
-	use {
-		'nvim-telescope/telescope.nvim',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}	
-	
-	use {
-		'nvim-treesitter/nvim-treesitter',
-		run = ':TSUpdate',
-		config = treeSitterConfig
-	}
+local nulllsConfig = function()
+	require("null-ls").config({
+		sources = {
+			require("null-ls").builtins.formatting.prettierd,
+			require("null-ls").builtins.formatting.stylua,
+		},
+	})
 
+	require("lspconfig")["null-ls"].setup({
+		on_attach = function(client)
+			if client.resolved_capabilities.document_formatting then
+				vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
+			end
+		end,
+	})
+end
 
-	use {
-	    'kyazdani42/nvim-tree.lua',
-	    requires = 'kyazdani42/nvim-web-devicons',
-	    config = function() require'nvim-tree'.setup{} end
-	}
-	use 'folke/tokyonight.nvim'
+require("packer").startup(function()
+	use("wbthomason/packer.nvim")
+	use("neovim/nvim-lspconfig")
+	use("hrsh7th/nvim-compe")
+	use("neoclide/vim-jsx-improve")
+	use("ggandor/lightspeed.nvim")
+	use("folke/tokyonight.nvim")
+	use("folke/which-key.nvim")
+
+	use({
+		"nvim-telescope/telescope.nvim",
+		requires = "nvim-lua/plenary.nvim",
+	})
+
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = ":TSUpdate",
+		config = treeSitterConfig,
+	})
+
+	use({
+		"kyazdani42/nvim-tree.lua",
+		requires = "kyazdani42/nvim-web-devicons",
+		config = function()
+			require("nvim-tree").setup({})
+		end,
+	})
+
+	use({
+		"jose-elias-alvarez/null-ls.nvim",
+		config = nulllsConfig,
+		requires = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+	})
 end)
-
