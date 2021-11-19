b49a7290a635737436178fa2a2c2a641c819bae4
renzhentaxi baerde <renzhentaxibaerde@gmail.com>
2021-11-19 16:58:06 -0500

    wip lua lsp

--- scripts/ensure_installed.bash
+++ scripts/ensure_installed.bash
@@ -1,11 +1,43 @@
 #!/usr/bin/env bash
 
+TB_REPO_DIRECTORY=$HOME/git_repos
+
 isNotInstalled() {
 	if which $1 >/dev/null 2>&1; then
 		return 1
 	else
 		return 0
 	fi
+}
+
+ensureGitRepo() {
+	local repoURL=$1
+	local repoName
+	if [ ! -z $2 ]; then
+		repoName=$2
+	else
+		repoName=$(basename $1)
+	fi
+
+	mkdir -p $TB_REPO_DIRECTORY
+
+	local repoDirectory=$TB_REPO_DIRECTORY/$repoName
+	cd $TB_REPO_DIRECTORY
+
+	if [ -d $repoName ]; then
+		cd $repoName
+		git pull
+		cd ..
+	else
+		git clone $repoURL $repoName
+	fi
+
+	cd $repoName
+	if [ -f .gitmodules ]; then
+		git submodule update --init --recursive
+	fi
+	cd ..
+
 }
 
 ensureInstalledSingle() {
@@ -32,6 +64,16 @@ ensureInstalledSingle() {
 			go install mvdan.cc/sh/v3/cmd/shfmt@latest
 		fi
 		;;
+	lua-language-server)
+		ensureGitRepo https://github.com/sumneko/lua-language-server
+		# git clone https://github.com/sumneko/lua-language-server
+		# cd lua-language-server
+		# git submodule update --init --recursive
+		# cd 3rd/luamake
+		# ./compile/install.sh
+		# cd ../..
+		# ./3rd/luamake/luamake rebuild
+		;;
 	*)
 		echo "unknown dep $1"
 		return 1
@@ -47,7 +89,8 @@ ensureInstalled() {
 	done
 }
 
-# lua formatter
-ensureInstalled stylua
+# lua
+ensureInstalled stylua lua-language-server
+
 # bash formatter
 ensureInstalled shfmt
