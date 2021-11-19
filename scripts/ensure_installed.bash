ed180ed38a183b055a119b8e6607a8ff726cd3b2
renzhentaxi baerde <renzhentaxibaerde@gmail.com>
2021-11-19 12:25:42 -0500

    ensure shfmt is installed; todo ensure go

--- scripts/ensure_installed.bash
+++ scripts/ensure_installed.bash
@@ -1,41 +1,53 @@
 #!/usr/bin/env bash
 
-isNotInstalled()
-{
-    if which $1 >/dev/null 2>&1; then
-        return 1
-    else
-        return 0
-    fi
+isNotInstalled() {
+	if which $1 >/dev/null 2>&1; then
+		return 1
+	else
+		return 0
+	fi
 }
 
-ensureInstalledSingle()
-{
-    case $1 in
-        rust)
-            if isNotInstalled rustup; then
-                curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-            fi
-            ;;
-        stylua)
-            ensureInstalled rust
-            if isNotInstalled $1; then
-                isNotInstalled $1 && cargo install $1
-            fi
-            ;;
-        *)
-            echo "unknown dep $1"
-            return 1
-    esac
-    echo ensured $1
+ensureInstalledSingle() {
+	case $1 in
+	rust | cargo)
+		if isNotInstalled rustup; then
+			curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+		fi
+		;;
+	stylua)
+		ensureInstalled rust
+		if isNotInstalled $1; then
+			cargo install $1
+		fi
+		;;
+	go)
+		if isNotInstalled go; then
+			echo todo
+		fi
+		;;
+	shfmt)
+		ensureInstalled go
+		if isNotInstalled shfmt; then
+			go install mvdan.cc/sh/v3/cmd/shfmt@latest
+		fi
+		;;
+	*)
+		echo "unknown dep $1"
+		return 1
+		;;
+	esac
+	echo ensured $1
 }
 
-ensureInstalled()
-{
-    local var
-    for var in "$@"; do
-        ensureInstalledSingle $var
-    done
+ensureInstalled() {
+	local var
+	for var in "$@"; do
+		ensureInstalledSingle $var
+	done
 }
 
-ensureInstalled rust stylua
+# lua formatter
+ensureInstalled stylua
+# bash formatter
+ensureInstalled shfmt
