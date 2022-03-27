aceb3be006cda3da52dfce1b2f83473efce6a591
taxi <renzhentaxibaerde@gmail.com>
2022-03-27 12:30:07 -0400

    basic support for rust

--- nvim/lua/lsp_setup.lua
+++ nvim/lua/lsp_setup.lua
@@ -1,7 +1,5 @@
 local nvim_lsp = require("lspconfig")
 
---local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
-
 local typescript = {
 	flags = { debounce_text_changes = 150 },
 	on_attach = function(client, bufnr)
@@ -10,11 +8,13 @@ local typescript = {
 		client.resolved_capabilities.document_range_formatting = false
 	end,
 }
+nvim_lsp.tsserver.setup(typescript)
 
 local golang = {
 	capabilities = capabilities,
 	flags = { debounce_text_changes = 150 },
 }
-nvim_lsp.tsserver.setup(typescript)
 nvim_lsp.gopls.setup(golang)
+
 nvim_lsp.eslint.setup({})
+nvim_lsp.rust_analyzer.setup({})
