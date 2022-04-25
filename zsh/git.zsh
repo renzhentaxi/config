beb0cb0b3269cf8cd193a15ccff1d83d7421159e
taxi <renzhentaxibaerde@gmail.com>
2022-04-25 00:19:34 -0400

    easier syntax for checkingout/creating new worktree/branch

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -28,26 +28,46 @@ has_remote_branch()
     return $?
 }
 
+safe_branch_name()
+{
+    echo $(echo $1 | tr '/' '-')
+}
+
 git_worktree_checkout()
 {
     if ! is_git_repo .; then
         echo 'not in a git repo!'
         return 1
     fi
-
-    worktreeDir=../$1
-    rest=($*[2,-2])
-    branchName=$(stripName $*[-1])
-
-    cd $(git worktree list | head -n 1 | cut -d " " -f1)
-    git pull
-     
-    git worktree add $worktreeDir $rest $branchName
-
+    
+    if [ "$#" -eq 0 ]; then
+        remote_branches=$(git branch -r --format "%(refname:short)" | cut -d/ -f2-)
+        local_branches=$(git branch --format "%(refname:short)")
+        all_branches=$({echo $remote_branches; echo $local_branches} | sort | uniq)
+        
+        branchName=$(echo $all_branches | fzf)
+    else 
+        if [ $1 = '-b' ]; then
+            is_new_branch='-b'
+            branchName=$(stripName $2)
+        else
+            is_new_branch=''
+            branchName=$(stripName $1)
+        fi
+    fi
+    
+    worktreeDir=../$(safe_branch_name $branchName)
+    
+    git worktree add $worktreeDir $is_new_branch $branchName
+    if [ $? -ne 0 ] ; then
+        return 1
+    fi
+    
+    # symlink node modules
     sourceNodeModule=$(realpath node_modules)
     targetNodeModule=$(realpath $worktreeDir/node_modules)
     ln -s $sourceNodeModule $targetNodeModule
-
+    
     cd $worktreeDir
     return
 }
