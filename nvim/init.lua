d4385e9e9d061e61966663692c526bcb4f719c7d
taxi <renzhentaxibaerde@gmail.com>
2023-01-26 20:49:55 -0500

    starting migrating to lazy

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,5 @@
-require("plugins")
+require("lazy_config")
+-- require("plugins")
 -- require("lsp_setup")
-require("general_setup")
-require("keymap")
+-- require("general_setup")
+-- require("keymap")
