624de1baa48ec032cc24bdda0cf4a4fd87b8e95e
taxi <renzhentaxibaerde@gmail.com>
2023-01-31 14:27:26 -0500

    added colorscheme and neotree

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,5 @@
 require("lazy_config")
 -- require("plugins")
 -- require("lsp_setup")
--- require("general_setup")
+require("general_setup")
 -- require("keymap")
--- nvim/lua/general_setup.lua
+++ nvim/lua/general_setup.lua
@@ -1,4 +1,4 @@
-vim.cmd([[colorscheme kanagawa]])
+vim.cmd([[colorscheme tokyonight]])
 
 vim.opt.completeopt = { "menu", "menuone", "noselect" }
 
