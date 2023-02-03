25d6d3a5ab5bdf400baee182984d1c44108dc9d6
taxi <renzhentaxibaerde@gmail.com>
2023-02-03 12:50:56 -0500

    shortcut to save file via space s

--- nvim/lua/new_keymap.lua
+++ nvim/lua/new_keymap.lua
@@ -1,9 +1,12 @@
 vim.g.mapleader = " "
 
+-- save
+vim.keymap.set("n", "<leader>s", ":w<CR>")
+
 -- open neotree
 vim.keymap.set("n", "\\", ":Neotree reveal toggle left<CR>")
 
 -- fuzzy find files
 vim.keymap.set("n", "<leader>p", function()
-    require("telescope.builtin").find_files()
+	require("telescope.builtin").find_files()
 end)
