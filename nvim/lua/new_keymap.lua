77fd97f9987f63ac91c48325d614668460e13632
taxi <renzhentaxibaerde@gmail.com>
2023-02-03 16:00:30 -0500

    added keymaps for viewing navigating diagnostics

--- nvim/lua/new_keymap.lua
+++ nvim/lua/new_keymap.lua
@@ -10,3 +10,11 @@ vim.keymap.set("n", "\\", ":Neotree reveal toggle left<CR>")
 vim.keymap.set("n", "<leader>p", function()
 	require("telescope.builtin").find_files()
 end)
+
+-- diagnostics
+vim.keymap.set("n", "<leader>d", vim.diagnostic.goto_next)
+vim.keymap.set("n", "<leader>D", vim.diagnostic.goto_prev)
+
+vim.keymap.set("n", "<leader>fd", function()
+	require("telescope.builtin").diagnostics()
+end)
