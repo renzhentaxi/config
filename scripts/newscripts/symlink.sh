216a48fea67ff83478db69ec95ef4efd777e3b8a
taxi <taxi@taxis-MBP.home>
2023-04-27 12:02:36 -0400

    basic alacritty config

--- scripts/newscripts/symlink.sh
+++ scripts/newscripts/symlink.sh
@@ -5,9 +5,16 @@ mkdir -p ~/.config
 cd ~/.config
 
 echo "Symlinking config"
+
+echo "nvim"
 ln -sf $currentDir/nvim
 
+echo "tmux"
 ln -sf $currentDir/tmux.conf ~/.tmux.conf 
 cd $currentDir  
 
+echo "alacritty"
+mkdir -p ~/.config/alacritty
+ln -sf $currentDir/configs/alacritty.yml ~/.config/alacritty/alacritty.yml
 
+echo "done"
